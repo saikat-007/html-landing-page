abe0dd3b98f0fe24d3d1036b2761fad5b291d4c5
saikat <saikatbanerjee124@gmail.com>
2024-08-07 21:52:52 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 document.getElementById('clickMeButton').addEventListener('click', function() {
     alert('Button clicked!');
 });
+
+document.getElementById('learnMoreButton').addEventListener('click', function() {
+    document.getElementById('home').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent!');
+});
